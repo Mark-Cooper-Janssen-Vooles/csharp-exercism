23072d35e4d950df7a922ab7ce937fb07edd6e13
Mark <markke4@outlook.com>
2019-12-03 12:26:04 +1100

    exercism challenges

---
+++ pangram/Pangram.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class Pangram
+{
+    public static bool IsPangram(string input)
+    {
+        Console.WriteLine("-------------------");
+        //first check if input is a blank string.
+        if(input == "")
+            return false;
+
+        //create list of all characters of alphabet.
+        var listOfLetters = new List<string>();
+        for ( int i = 0; i < 26; i++ )
+        {
+            var letter = Convert.ToChar( i + 65 );
+            listOfLetters.Add( Convert.ToString(letter).ToLower() );
+        }
+
+        var listOfInput = new List<string>();
+        //convert "input" string to List 
+        for(int i = 0; i < input.Length; i++){
+            listOfInput.Add(Convert.ToString(input[i]).ToLower());
+        }
+
+        //check if input contains the letters of the alphabet, checking all letters.
+        foreach(var letter in listOfLetters)
+        {
+            Console.WriteLine(letter);
+            if(!listOfInput.Contains(letter))
+            {
+                return false;
+            }
+        }
+        //if code reaches this point, return true.
+        return true;
+
+        throw new NotImplementedException();
+    }
+}
--- two-fer/TwoFer.cs
+++ two-fer/TwoFer.cs
@@ -4,11 +4,16 @@ public static class TwoFer
 {
     // In order to get the tests running, first you need to make sure the Speak method 
     // can be called both without any arguments and also by passing one string argument.
-    public static string Speak()
+    public static string Speak(string name = "")
     {
         // not sure of syntax. watch videos in readme. 
-        // //if string blank
-        // return 'One for you, one for me.';
+        if (name == "") 
+        {
+            return "One for you, one for me.";
+        } else 
+        {
+            return $"One for {name}, one for me.";
+        }
         // //if string has an entry 
         // return `One for ${}, one for me.~;
     }
